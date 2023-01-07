c3972ff0e2db14c95211797e381e640da2add288
Stefan Vukovic <stefanvukovic44@gmail.com>
2023-01-07 14:17:45 +0100

    account activation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -17,17 +18,56 @@ app.get("/", (req, response) => {
     response.send("Hello world!");
 });
 
-app.post("/register", async (req, response) => {
-    const newUser = req.body;
-    newUser.password = bcrypt.hashSync( newUser.password, 10 )
-    
-    User.create(newUser)
-    .then((createdUser: any) => {
-      response.status(201).json(createdUser);
-    })
-    .catch((err: any)=>{
-      response.status(500).json(err);
-    })
+app.post("/register", async (req, res) => {
+  const newUser = req.body;
+  newUser.password = bcrypt.hashSync(newUser.password, 10);
+  newUser.active = 'unactivated'; // add this field to track whether the user has activated their account
+
+  try {
+    const createdUser = await User.create(newUser);
+
+    const activationToken = jwt.sign(
+      { id: createdUser.id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT, 10), 
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD
+      }
+    });
+
+    const mailOptions = {
+      from: "welcome@lastdrop.com",
+      to: newUser.email,
+      subject: "Account activation",
+      html: `<p>Click the link below to activate your account:</p>
+             <p><a href="${process.env.SERVER}/activate/${activationToken}">Activate my account</a></p>`
+    };
+    await transporter.sendMail(mailOptions);
+
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/activate/:token", async (req, res) => {
+  const { token } = req.params;
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_SECRET as string) as { id: number };
+    await User.update({ active: 'activated' }, { where: { id } });
+
+    res.redirect(`${process.env.CLIENT}/login`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.post("/login", async (req, res) => {
@@ -43,6 +83,10 @@ app.post("/login", async (req, res) => {
       const passwordMatch = bcrypt.compareSync(password, user.password);
       if (!passwordMatch) {
         return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      if (user.active === 'unactivated') {
+        return res.status(401).json({ message: "You have not activated your account" });
       }
 
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
